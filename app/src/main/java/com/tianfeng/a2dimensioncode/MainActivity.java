7ba5ff37255dc88fab0cf78c98a9674089d2ea3d
卢渊 <yuanlua@isoftstone.com>
2016-06-23 15:41:21 +0800

    [Featrue]完成二维码扫描,条码扫描,扫描结果处理

--- app/src/main/java/com/tianfeng/a2dimensioncode/MainActivity.java
+++ app/src/main/java/com/tianfeng/a2dimensioncode/MainActivity.java
@@ -1,17 +1,27 @@
 package com.tianfeng.a2dimensioncode;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.client.android.CaptureActivity;
+import com.google.zxing.client.android.Intents;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mBtnScan;
+    private TextView mEtText;
+    private EditText mEtInput;
+    private Button mBtnMake;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView() {
         mBtnScan = (Button) findViewById(R.id.btn_scan);
-
+        mEtText = (TextView) findViewById(R.id.et_text);
+        imageView = (ImageView) findViewById(R.id.iv_image);
+        mBtnMake = (Button) findViewById(R.id.btn_make);
+        mEtInput = (EditText) findViewById(R.id.et_input);
+        String str =  mEtInput.getText().toString().trim();
         mBtnScan.setOnClickListener(this);
+        mBtnMake.setOnClickListener(this);
     }
 
     @Override
@@ -31,12 +46,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch(id){
             case R.id.btn_scan:
-                Toast.makeText(this,"你可以扫描条码和二维码了",Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"你可以扫描条码和二维码了",Toast.LENGTH_SHORT).show();
                 Intent startScan = new Intent(MainActivity.this,CaptureActivity.class);
-                startActivity(startScan);
+                //startActivity(startScan);
+                startActivityForResult(startScan,0);
+                break;
+            case R.id.btn_make:
+                Toast.makeText(this,"生成二维码",Toast.LENGTH_SHORT).show();
+                Bitmap reqbmp;
                 break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_OK){
+            String result = data.getExtras().getString("result");
+            mEtText.setText(result);
         }
     }
 }
